91d1ab99445e197b955066528d420cb3640935e5
Ivan A. Reyes <ivan.reor@gmail.com>
2019-11-16 19:14:18 -0500

    Implemented 360-exploration to the image collection

--- drone_image_collection.py
+++ drone_image_collection.py
@@ -1,23 +1,34 @@
 """
+IMAGE COLLECTION
+--------------------------------------------------------------------------------------------------------------
 ADAPTED FROM: 
     Availability: https://github.com/simondlevy/AirSimTensorFlow/blob/master/image_collection.py
     Title: image_collection.py
     Authors: Jack Baird, Alex Cantrell, Keith Denning, Rajwol Joshi, 
     Simon D. Levy, Will McMurtry, Jacob Rosen
-
 CHANGES FROM ABOVE: 
     Changed airsim client from CarClient to MultirotorClient, and also took out enableAPIControl to be able
     to gather the collision images from collisions made by piloting the multirotor manually, using a RC.
     Instead of terminating after a collision, the code sleeps to allow the pilot to move away from the obstacle 
     and the next set of images are collected upon the next collision, and so on.
-
 CHANGES MADE BY: 
     Ernest Quant
+
+DRONE 360-MOVEMENT & Collection
+--------------------------------------------------------------------------------------------------------------
+DESCRIPTION:
+    Added continuous collection of training images by allowing the drone to explore a linear path from origin 
+    to the boundaries of the environment. Each exploration path is rotated 5 degrees (counterclockwise). 
+    Increased QUEUESIZE to 100 to allow the collection of more imges prior to a collition, including SAFE 
+    situations. 
+CHANGES MADE BY: 
+    Ivan A. Reyes
 """
 import airsim
 import pprint
 import time
 import os
+import math
 
 
 
@@ -35,45 +46,59 @@ except OSError:
 
 
 imagequeue = []
-QUEUESIZE = 5       # NUMBER OF IMAGES KEPT RIGHT BEFORE AND UP TO A COLLISION
+QUEUESIZE = 200       # NUMBER OF IMAGES KEPT RIGHT BEFORE AND UP TO A COLLISION
 imageBatch = 0      # USED TO INCREMENT FILE NUMBERS AFTER COLLISION
 #endNum = 100       # IF YOU WANT TO TERMINATE AT A CERTAIN NUMBER OF IMAGES
 
 
 while True:
 
-    while True:
+    #INSERTED HERE
+    #Automates the position and data collection
+    airsim.wait_key('Press any key to start 360-exploration starting from Origin at 5 m/s')
+    degrees = list(range(0,360,5)) #from 0 to 
+    travel_distance = 20 #FIX (More appropiate travel distance)
+    for degree in degrees:
+        xcoor = travel_distance * math.cos(math.radians(degree))
+        ycoor = travel_distance * math.sin(math.radians(degree))
+        #Assuming (x,y,z,v)
+        client.moveToPositionAsync(xcoor, ycoor, 5  , 5).join()
+        client.hoverAsync().join()
 
-        responses = client.simGetImages([airsim.ImageRequest(1,airsim.ImageType.Scene)])
+    #END OF INSERTION
 
-        imagequeue.append(responses[0].image_data_uint8)
+        while True:
 
-        # KEEPS THE IMAGE QUEUE POPULATED WITH THE MOST RECENT 5 IMAGES
-        if len(imagequeue) == QUEUESIZE:
-            for i in range(QUEUESIZE):
-                filename = os.path.join(tmp_dir, str(i + imageBatch))
-                airsim.write_file(os.path.normpath(filename + '.png'),imagequeue[i])
-            imagequeue.pop(0)
+            responses = client.simGetImages([airsim.ImageRequest(1,airsim.ImageType.Scene)])
 
-        collision_info = client.simGetCollisionInfo()
+            imagequeue.append(responses[0].image_data_uint8)
 
-        # PRINTS COLLISION INFORMATION AFTER COLLISION AND THEN BREAKS
-        if collision_info.has_collided:
-            print("Collision at pos %s, normal %s, impact pt %s, penetration %f, name %s, obj id %d" % (
-                pprint.pformat(collision_info.position), 
-                pprint.pformat(collision_info.normal), 
-                pprint.pformat(collision_info.impact_point), 
-                collision_info.penetration_depth, collision_info.object_name, collision_info.object_id))
-            break
+            # KEEPS THE IMAGE QUEUE POPULATED WITH THE MOST RECENT IMAGES
+            if len(imagequeue) == QUEUESIZE:
+                for i in range(QUEUESIZE):
+                    filename = os.path.join(tmp_dir, str(i + imageBatch))
+                    airsim.write_file(os.path.normpath(filename + '.png'),imagequeue[i])
+                imagequeue.pop(0)
 
-        time.sleep(0.1)
+            collision_info = client.simGetCollisionInfo()
+            # PRINTS COLLISION INFORMATION AFTER COLLISION AND THEN BREAKS
+            if collision_info.has_collided:
+                print("Collision at pos %s, normal %s, impact pt %s, penetration %f, name %s, obj id %d" % (
+                    pprint.pformat(collision_info.position), 
+                    pprint.pformat(collision_info.normal), 
+                    pprint.pformat(collision_info.impact_point), 
+                    collision_info.penetration_depth, collision_info.object_name, collision_info.object_id))
+                break
 
-    # UNCOMMENT TO TERMINATE AFTER A SET NUMBER OF IMAGES
-    """if imageBatch > endNum:
-        break"""
+            time.sleep(0.1)
 
-    imageBatch += 5
-    time.sleep(5)
+        # UNCOMMENT TO TERMINATE AFTER A SET NUMBER OF IMAGES
+        """if imageBatch > endNum:
+            break"""
+
+        imageBatch += QUEUESIZE
+        client.reset()
+        time.sleep(1)
 
 
 client.reset()
