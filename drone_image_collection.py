61659416411d4dddd80fe2590fc928d8b0e2fbb4
Orson Meyreles <35610902+Orfonso@users.noreply.github.com>
2019-11-17 17:38:12 -0500

    Update drone_image_collection.py
    NOT WORKING: Currently puts images in both directories, regardless of collision.

--- drone_image_collection.py
+++ drone_image_collection.py
@@ -1,49 +1,50 @@
 """
-IMAGE COLLECTION
+IMAGE COLLECTION & DRONE 360-EXPLORATION
 --------------------------------------------------------------------------------------------------------------
-ADAPTED FROM: 
-    Availability: https://github.com/simondlevy/AirSimTensorFlow/blob/master/image_collection.py
-    Title: image_collection.py
-    Authors: Jack Baird, Alex Cantrell, Keith Denning, Rajwol Joshi, 
-    Simon D. Levy, Will McMurtry, Jacob Rosen
-CHANGES FROM ABOVE: 
+DESCRIPTION:
     Changed airsim client from CarClient to MultirotorClient, and also took out enableAPIControl to be able
     to gather the collision images from collisions made by piloting the multirotor manually, using a RC.
     Instead of terminating after a collision, the code sleeps to allow the pilot to move away from the obstacle 
     and the next set of images are collected upon the next collision, and so on.
-CHANGES MADE BY: 
-    Ernest Quant
-
-DRONE 360-EXPLORATION
---------------------------------------------------------------------------------------------------------------
-DESCRIPTION:
+    
     Added continuous collection of training images by allowing the drone to explore a linear path from origin 
     to the boundaries of the environment. Each exploration path is rotated 5 degrees (counterclockwise). 
-    Increased QUEUESIZE to 100 to allow the collection of more imges prior to a collition, including SAFE 
-    situations. 
-CHANGES MADE BY: 
+    Increased QUEUESIZE to 200 to allow the collection of more imges prior to a collition, including SAFE 
+    situations. Added a timer to proceed to next path after enough time in collition-free trayectory.
+--------------------------------------------------------------------------------------------------------------    
+MADE BY: 
     Ivan A. Reyes
+    Ernest J. Quant
+    Orson Meyreles
+    John Quitto-Graham  
+--------------------------------------------------------------------------------------------------------------
+
 """
+import setup_path
 import airsim
 import pprint
 import time
 import os
 import math
 
-
-
 client = airsim.MultirotorClient()
 client.confirmConnection()
 client.enableApiControl(True)
 client.armDisarm(True)
 
-# CHANGE TO YOUR DESIRED DIRECTORY
-tmp_dir = r"C:\Users\Ernest\Pictures\Collision_Images"
+# Directory to save non-collision images (Safe)
+tmp_dir_safe = r"C:\Users\Not Orson\Pictures\Collision_Images\Safe"
+
+# Directory to save collision images (Unsafe)
+tmp_dir_unsafe = r"C:\Users\Not Orson\Pictures\Collision_Images\Unsafe"
 
 try:
-    os.makedirs(tmp_dir)
+    os.makedirs(tmp_dir_safe)
+    os.makedirs(tmp_dir_unsafe)
 except OSError:
-    if not os.path.isdir(tmp_dir):
+    if not os.path.isdir(tmp_dir_safe):
+        raise
+    if not os.path.isdir(tmp_dir_unsafe):
         raise
 
 
@@ -53,33 +54,50 @@ imageBatch = 0  # USED TO INCREMENT FILE NUMBERS AFTER COLLISION
 #endNum = 100   # IF YOU WANT TO TERMINATE AT A CERTAIN NUMBER OF IMAGES
 
 degrees = list(range(0,360,5)) #from 0 to 360 in intervals of 5
-travel_distance = 50 
+velocityMultiplayer = 10
 airsim.wait_key('Press any key to take off and start 360-exploration starting from Origin (height = 20m) at 15 m/s')
 
 while True:
-
+    #Automates the position and data collection
     for degree in degrees:
-        #Automates the position and data collection
-        client.takeoffAsync().join()
-        xcoor = travel_distance * math.cos(math.radians(degree))
-        ycoor = travel_distance * math.sin(math.radians(degree))
-        client.moveToPositionAsync(xcoor, ycoor, -20  , 15)
-        client.hoverAsync().join()
         
-        while True:
+        print("Preparing..", end='.')
+        client.armDisarm(True)#ADDED NOW
+        client.enableApiControl(True)
+        client.takeoffAsync().join()
+
+        print("Ready!")
+        timeout = 15   #in seconds
+        timeout_start = time.time()
+        xcoor = velocityMultiplayer * math.cos(math.radians(degree))
+        ycoor = velocityMultiplayer * math.sin(math.radians(degree))
+        client.moveByVelocityAsync(xcoor, ycoor, 0, 20)
+        
+        
+        while time.time() < timeout_start + timeout:
 
             responses = client.simGetImages([airsim.ImageRequest(1,airsim.ImageType.Scene)])
 
             imagequeue.append(responses[0].image_data_uint8)
 
+            collision_info = client.simGetCollisionInfo()
             # KEEPS THE IMAGE QUEUE POPULATED WITH THE MOST RECENT IMAGES
             if len(imagequeue) == QUEUESIZE:
-                for i in range(QUEUESIZE):
-                    filename = os.path.join(tmp_dir, str(i + imageBatch))
-                    airsim.write_file(os.path.normpath(filename + '.png'),imagequeue[i])
+                # if drone colides
+                if collision_info.has_collided:
+                    for i in range(QUEUESIZE):
+                        filename = os.path.join(tmp_dir_unsafe, str(i + imageBatch))
+                        airsim.write_file(os.path.normpath(filename + '.png'),imagequeue[i])
+                    print("Writing to unsafe")
+                    
+                # if drone doesn't colide
+                else:
+                    for i in range(QUEUESIZE):
+                        filename = os.path.join(tmp_dir_safe, str(i + imageBatch))
+                        airsim.write_file(os.path.normpath(filename + '.png'),imagequeue[i])
+                    print("Writing to safe")
                 imagequeue.pop(0)
-
-            collision_info = client.simGetCollisionInfo()
+            
             # PRINTS COLLISION INFORMATION AFTER COLLISION AND THEN BREAKS
             if collision_info.has_collided:
                 print("Collision at pos %s, normal %s, impact pt %s, penetration %f, name %s, obj id %d" % (
@@ -89,13 +107,35 @@ while True:
                     collision_info.penetration_depth, collision_info.object_name, collision_info.object_id))
                 break
 
-            time.sleep(0.1)
+        
 
+        print("Disarming...")
+        client.armDisarm(False)
+            
         # UNCOMMENT TO TERMINATE AFTER A SET NUMBER OF IMAGES
         """if imageBatch > endNum:
             break"""
+        
+        
 
+        print("Restarting!")
         imageBatch += QUEUESIZE
         client.reset()
 
+        
+
+
 client.reset()
+
+
+'''
+--------------------------------------------------------------------------------------------------------------    
+
+INSPIRED FROM:
+    Availability: https://github.com/simondlevy/AirSimTensorFlow/blob/master/image_collection.py
+    Title: image_collection.py
+    Authors: Jack Baird, Alex Cantrell, Keith Denning, Rajwol Joshi, Simon D. Levy, Will McMurtry, Jacob Rosen    
+    
+--------------------------------------------------------------------------------------------------------------
+'''
+
