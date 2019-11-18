ed519af2f3afba7c6983ccdf8a1f5956a0695fc9
ItsLaro <ivan.reor@gmail.com>
2019-11-17 23:19:55 -0500

    Adjusted velocity variable and variable names

---
+++ drone_image_classifier.py
@@ -0,0 +1,56 @@
+"""
+Classifier using a convolutional network
+
+MADE BY: 
+    Ivan A. Reyes
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import cv2
+from tqdm import tqdm
+
+DATADIR = r"C:\Users\ivanr\OneDrive\Documents\FIUCS\SparkDev\ai-uav-simulator"
+
+CATEGORIES = ["Safe", "Unsafe"]
+
+for category in CATEGORIES:  # do dogs and cats
+    path = os.path.join(DATADIR,category)  # create path to dogs and cats
+    for img in os.listdir(path):  # iterate over each image per dogs and cats
+        img_array = cv2.imread(os.path.join(path,img) ,cv2.IMREAD_GRAYSCALE)  # convert to array
+        plt.imshow(img_array, cmap='gray')  # graph it
+        plt.show()  # display!
+
+        break  # we just want one for now so break
+    break  #...and one more!
+
+IMG_SIZE = 100
+
+new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+plt.imshow(new_array, cmap='gray')
+#plt.show()
+
+training_data = []
+
+def create_training_data():
+    for category in CATEGORIES:  # do dogs and cats
+
+        path = os.path.join(DATADIR,category)  # create path to dogs and cats
+        class_num = CATEGORIES.index(category)  # get the classification  (0 or a 1). 0=dog 1=cat
+
+        for img in tqdm(os.listdir(path)):  # iterate over each image per dogs and cats
+            try:
+                img_array = cv2.imread(os.path.join(path,img) ,cv2.IMREAD_GRAYSCALE)  # convert to array
+                new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))  # resize to normalize data size
+                training_data.append([new_array, class_num])  # add this to our training_data
+            except Exception as e:  # in the interest in keeping the output clean...
+                pass
+            #except OSError as e:
+            #    print("OSErrroBad img most likely", e, os.path.join(path,img))
+            #except Exception as e:
+            #    print("general exception", e, os.path.join(path,img))
+
+create_training_data()
+
+print(len(training_data))
