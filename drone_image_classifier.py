3cc2b3be9446c0e5bfad0987f020f2914862e96b
jquitgra <45401768+jquitgra@users.noreply.github.com>
2019-11-18 13:15:31 -0500

    Testing a dataset saver from https://pythonprogramming.net/neural-networks-machine-learning-tutorial/

--- drone_image_classifier.py
+++ drone_image_classifier.py
@@ -1,19 +1,12 @@
-"""
-Classifier using a convolutional network
-
-MADE BY: 
-    Ivan A. Reyes
-"""
-
 import numpy as np
 import matplotlib.pyplot as plt
 import os
 import cv2
 from tqdm import tqdm
 
-DATADIR = r"C:\Users\ivanr\OneDrive\Documents\FIUCS\SparkDev\ai-uav-simulator"
+DATADIR = "X:/Datasets/PetImages"
 
-CATEGORIES = ["Safe", "Unsafe"]
+CATEGORIES = ["Dog", "Cat"]
 
 for category in CATEGORIES:  # do dogs and cats
     path = os.path.join(DATADIR,category)  # create path to dogs and cats
@@ -25,11 +18,87 @@ for category in CATEGORIES:  # do dogs and cats
         break  # we just want one for now so break
     break  #...and one more!
 
-IMG_SIZE = 100
+print(img_array)
+
+print(img_array.shape)
+
+IMG_SIZE = 50
 
 new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
 plt.imshow(new_array, cmap='gray')
-#plt.show()
+plt.show()
+
+
+search
+Home
++=1
+Support the Content
+Community
+Log in
+Sign up
+
+Loading in your own data - Deep Learning basics with Python, TensorFlow and Keras p.2
+
+
+
+
+Welcome to a tutorial where we'll be discussing how to load in our own outside datasets, which comes with all sorts of challenges!
+
+First, we need a dataset. Let's grab the Dogs vs Cats dataset from Microsoft. If this dataset disappears, someone let me know. I will host it myself.
+
+Now that you have the dataset, it's currently compressed. Unzip the dataset, and you should find that it creates a directory called PetImages. Inside of that, we have Cat and Dog directories, which are then filled with images of cats and dogs. Easy enough! Let's play with this dataset! First, we need to understand how we will convert this dataset to training data. We have a few issues right out of the gate. The largest issue is not all of these images are the same size. While we can eventually have variable-sized layers in neural networks, this is not the most basic thing to achieve. We're going to want to reshape things for now so every image has the same dimensions. Next, we may or may not want to keep color. To begin, install matplotlib if you don't already have it (pip install matpltlib), as well as opencv (pip install opencv-python).
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import cv2
+from tqdm import tqdm
+
+DATADIR = "X:/Datasets/PetImages"
+
+CATEGORIES = ["Dog", "Cat"]
+
+for category in CATEGORIES:  # do dogs and cats
+    path = os.path.join(DATADIR,category)  # create path to dogs and cats
+    for img in os.listdir(path):  # iterate over each image per dogs and cats
+        img_array = cv2.imread(os.path.join(path,img) ,cv2.IMREAD_GRAYSCALE)  # convert to array
+        plt.imshow(img_array, cmap='gray')  # graph it
+        plt.show()  # display!
+
+        break  # we just want one for now so break
+    break  #...and one more!
+
+Oh look, a dog!
+
+print(img_array)
+[[189 189 189 ...  29  29  31]
+ [186 186 186 ...  36  35  36]
+ [184 185 185 ...  35  33  33]
+ ...
+ [168 169 170 ...  71  72  72]
+ [169 170 171 ...  68  67  67]
+ [168 169 170 ...  64  63  62]]
+And now it's shape:
+
+print(img_array.shape)
+(398, 500)
+So that's a 375 tall, 500 wide, and 3-channel image. 3-channel is because it's RGB (color). We definitely don't want the images that big, but also various images are different shapes, and this is also a problem.
+
+IMG_SIZE = 50
+
+new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+plt.imshow(new_array, cmap='gray')
+plt.show()
+
+Hmm, that's a bit blurry I'd say. Let's go with 100x100?
+
+new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+plt.imshow(new_array, cmap='gray')
+plt.show()
+
+Better. Let's try that. Next, we're going to want to create training data and all that, but, first, we should set aside some images for final testing. I am going to just manually create a directory called Testing and then create 2 directories inside of there, one for Dog and one for Cat. From here, I am just going to move the first 15 images from both Dog and Cat into the training versions. Make sure you move them, not copy. We will use this for our final tests.
+
+Now, we want to begin building our training data!
 
 training_data = []
 
@@ -54,3 +123,38 @@ def create_training_data():
 create_training_data()
 
 print(len(training_data))
+
+import random
+
+random.shuffle(training_data)
+
+for sample in training_data[:10]:
+    print(sample[1])
+
+X = []
+y = []
+
+for features,label in training_data:
+    X.append(features)
+    y.append(label)
+
+print(X[0].reshape(-1, IMG_SIZE, IMG_SIZE, 1))
+
+X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+
+
+import pickle
+
+pickle_out = open("X.pickle","wb")
+pickle.dump(X, pickle_out)
+pickle_out.close()
+
+pickle_out = open("y.pickle","wb")
+pickle.dump(y, pickle_out)
+pickle_out.close()
+
+'''pickle_in = open("X.pickle","rb")
+X = pickle.load(pickle_in)
+
+pickle_in = open("y.pickle","rb")
+y = pickle.load(pickle_in)'''
