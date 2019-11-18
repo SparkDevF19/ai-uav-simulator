fc5f552668e59d8a8a9ef7c20593530f2a911540
jquitgra <45401768+jquitgra@users.noreply.github.com>
2019-11-18 14:20:06 -0500

    We are just testing for our labeling functionality

--- drone_image_classifier.py
+++ drone_image_classifier.py
@@ -1,56 +1,102 @@
-"""
-Classifier using a convolutional network
-
-MADE BY: 
-    Ivan A. Reyes
-"""
+'''This is not our work, we are simpliy modifying the CNN
+from https://pythonprogramming.net/convolutional-neural-network-cnn-machine-learning-tutorial/
+by: Harrison Kinsley'''
 
 import numpy as np
+import pickle
 import matplotlib.pyplot as plt
 import os
 import cv2
 from tqdm import tqdm
 
-DATADIR = r"C:\Users\ivanr\OneDrive\Documents\FIUCS\SparkDev\ai-uav-simulator"
+DATADIR = "" #Whater the directory to SAFE and UNSAFE images
+STATES = ["AirSim_safe", "Airsim_Unsafe"] #sub directories
 
-CATEGORIES = ["Safe", "Unsafe"]
+#function to convert to array for our visulization function
+def dataToAR():
 
-for category in CATEGORIES:  # do dogs and cats
-    path = os.path.join(DATADIR,category)  # create path to dogs and cats
-    for img in os.listdir(path):  # iterate over each image per dogs and cats
-        img_array = cv2.imread(os.path.join(path,img) ,cv2.IMREAD_GRAYSCALE)  # convert to array
-        plt.imshow(img_array, cmap='gray')  # graph it
-        plt.show()  # display!
+    for state in STATES:
+        path = os.path.join(DATADIR,state)  #iterate through the states to train the model
+        for img in os.listdir(path):  # iterate over each image
+            img_array = cv2.imread(os.path.join(path,img) ,cv2.IMREAD_GRAYSCALE)  # convert to array
+            '''plt.imshow(img_array, cmap='gray')  # graph it
+            plt.show()  # display!'''
+            break  # we just want one for now so break
+        break  #...and one more!
+    ImgAttributes(img_array)
 
-        break  # we just want one for now so break
-    break  #...and one more!
+#Helper function to help us visualize image attributes
+def ImgAttributes(image):
+    print(img_array)
+    [[189 189 189 ...  29  29  31]
+    [186 186 186 ...  36  35  36]
+    [184 185 185 ...  35  33  33]
+    ...
+    [168 169 170 ...  71  72  72]
+    [169 170 171 ...  68  67  67]
+    [168 169 170 ...  64  63  62]]
 
-IMG_SIZE = 100
+    print(img_array.shape)
+    (398, 500)
 
-new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
-plt.imshow(new_array, cmap='gray')
-#plt.show()
+    IMG_SIZE = 50
 
-training_data = []
+    new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+    plt.imshow(new_array, cmap='gray')
+    plt.show()
+
+    new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+    plt.imshow(new_array, cmap='gray')
+    plt.show()
+
+    training_data = []
 
 def create_training_data():
-    for category in CATEGORIES:  # do dogs and cats
+    for state in STATES:
+        path = os.path.join(DATADIR,state)  # create path to STATES[state]
+        class_num = STATES.index(state)  # get the classification  (0 or a 1).
 
-        path = os.path.join(DATADIR,category)  # create path to dogs and cats
-        class_num = CATEGORIES.index(category)  # get the classification  (0 or a 1). 0=dog 1=cat
-
-        for img in tqdm(os.listdir(path)):  # iterate over each image per dogs and cats
+        for img in tqdm(os.listdir(path)):  # iterate over each image
             try:
                 img_array = cv2.imread(os.path.join(path,img) ,cv2.IMREAD_GRAYSCALE)  # convert to array
                 new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))  # resize to normalize data size
                 training_data.append([new_array, class_num])  # add this to our training_data
             except Exception as e:  # in the interest in keeping the output clean...
                 pass
-            #except OSError as e:
-            #    print("OSErrroBad img most likely", e, os.path.join(path,img))
-            #except Exception as e:
-            #    print("general exception", e, os.path.join(path,img))
 
-create_training_data()
+def main():
+    create_training_data()
 
-print(len(training_data))
+    print(len(training_data))
+
+    import random
+
+    random.shuffle(training_data)
+
+    for sample in training_data[:10]:
+        print(sample[1])
+
+    X = []
+    y = []
+
+    for features,label in training_data:
+        X.append(features)
+        y.append(label)
+
+    print(X[0].reshape(-1, IMG_SIZE, IMG_SIZE, 1))
+
+    X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+
+    pickle_out = open("X.pickle","wb")
+    pickle.dump(X, pickle_out)
+    pickle_out.close()
+
+    pickle_out = open("y.pickle","wb")
+    pickle.dump(y, pickle_out)
+    pickle_out.close()
+
+    '''pickle_in = open("X.pickle","rb")
+    X = pickle.load(pickle_in)
+
+    pickle_in = open("y.pickle","rb")
+    y = pickle.load(pickle_in)'''
